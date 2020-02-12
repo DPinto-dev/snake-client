ed784330a530a4f494b3aabb2362b591810e7321
Diogo Pinto <diogosmp@gmail.com>
2020-02-12 22:22:07 +0000

    Created movement functionality

--- input.js
+++ input.js
@@ -1,23 +1,35 @@
+// Stores the active TCP connection object.
+let connection;
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", () => handleUserInput());
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
 const handleUserInput = data => {
   if (data === "\u0003") {
-    process.exit();
+    process.exit(0);
+  } else if (data === "k" || data === "w") {
+    connection.write("Move: up");
+    console.log("UP");
+  } else if (data === "j" || data === "s") {
+    connection.write("Move: down");
+    console.log("DOWN");
+  } else if (data === "h" || data === "a") {
+    connection.write("Move: left");
+    console.log("LEFT");
+  } else if (data === "l" || data === "d") {
+    connection.write("Move: right");
+    console.log("RIGHT");
   }
-  // } else {
-  //   // conn.write(data);
-  // }
 };
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -5,3 +5,5 @@ console.log("Connecting...");
 connect();
 
 setupInput();
+
+setupInput(connect());
